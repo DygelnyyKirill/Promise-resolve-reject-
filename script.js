6b7c01f191a453dae953d7ce336790f2e2b23da3
DygelnyyKirill <dygelniy.1994kirill@gmail.com>
2021-03-09 21:30:38 +0200

    getter/setter

--- script.js
+++ script.js
@@ -1,47 +1,47 @@
 'use strict'
 
-// filter
+filter
 
-// const names = ['Ivan', 'Ann', 'Petrov', 'Konstantin']
+const names = ['Ivan', 'Ann', 'Petrov', 'Konstantin']
 
-// const shortNames = names.filter(function(name) {
-//     return name.length < 5; 
-// });
+const shortNames = names.filter(function(name) {
+    return name.length < 5; 
+});
 
-// console.log(shortNames);
+console.log(shortNames);
 
-// map
+map
 
-// const answers = ['IvAn', 'AnnA', 'Hello'];
+const answers = ['IvAn', 'AnnA', 'Hello'];
 
-// const result = answers.map(item => item.toLocaleLowerCase());
+const result = answers.map(item => item.toLocaleLowerCase());
 
-// console.log(result);
+console.log(result);
 
-// every/some
+every/some
 
-// const some = [4, 'qwq', 'sdsacacc'];
+const some = [4, 'qwq', 'sdsacacc'];
 
-// console.log(some.some(item => typeof(item) === 'number'));
+console.log(some.some(item => typeof(item) === 'number'));
 
-// console.log(some.every(item => typeof(item) === 'number'));
+console.log(some.every(item => typeof(item) === 'number'));
 
-// console.log(some)
+console.log(some)
 
-// reduce
+reduce
 
-// const arr = [4, 5, 1, 3, 2, 6];
+const arr = [4, 5, 1, 3, 2, 6];
 
-// const res = arr.reduce((sum, current) => sum + current, 3);
+const res = arr.reduce((sum, current) => sum + current, 3);
 
-// console.log(res);
+console.log(res);
 
 
-// const arr = ['apple', 'pear', 'plum'];
+const arr = ['apple', 'pear', 'plum'];
 
-// const res = arr.reduce((sum, current) => `${sum}, ${current}`);
+const res = arr.reduce((sum, current) => `${sum}, ${current}`);
 
-// console.log(res);
+console.log(res);
 
 const obj = {
     ivan: 'persone',
@@ -51,5 +51,6 @@ const obj = {
 };
 
 const newArr = Object.entries(obj)
-.filter(item => item[1] === 'person');
-console.log(newArr)
+.filter(item => item[1] === 'persone')
+.map(item => item[0]);
+console.log(newArr);
